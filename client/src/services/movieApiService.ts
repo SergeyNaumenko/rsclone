dd3688124129ff1817c3e5301889e327cd8e95a1
Sergey Naumenko <sergeynau@gmail.com>
2021-01-14 03:44:55 +0300

    feat: add service

--- client/src/services/movieApiService.ts
+++ client/src/services/movieApiService.ts
@@ -1,6 +1,7 @@
 export default class MovieApiService {
   private apiKey: string = 'd59637b83d0355b53f5e01b07ba35228';
   private baseUrl: string = 'https://api.themoviedb.org/3/';
+  private baseParams: string = `api_key=${this.apiKey}&page=1&language=en-US`;
 
   private async getResource(path: string) {
     const url: string = `${this.baseUrl}${path}`;
@@ -15,13 +16,23 @@ export default class MovieApiService {
   }
 
   public async getGenres(type: string = 'movie') {
-    const path: string = `genre/${type}/list?api_key=${this.apiKey}`;
+    const path: string = `genre/${type}/list?${this.baseParams}`;
     const json = await this.getResource(path);
     return json.genres;
   }
 
   public async getTopRatedMovies() {
-    const path: string = `movie/top_rated?api_key=${this.apiKey}&page=1`;
+    const path: string = `movie/top_rated?${this.baseParams}&page=1`;
+    const json = await this.getResource(path);
+    return json.results.map(this.transformMovie);
+  }
+
+  public async getByGenres() {
+    return this.discoverMovies('&with_genres=28');
+  }
+
+  private async discoverMovies(params: string) {
+    const path = `discover/movie?${this.baseParams}&sort_by=popularity.desc&page=1`;
     const json = await this.getResource(path);
     return json.results.map(this.transformMovie);
   }
@@ -31,18 +42,18 @@ export default class MovieApiService {
 
     return {
       adult: movie.adult,
-      backdrop_path: `${imagesBaseUrl}${movie.backdrop_path}`,
-      genre_ids: movie.genre_ids,
+      backdropPath: `${imagesBaseUrl}${movie.backdrop_path}`,
+      genreIds: movie.genre_ids,
       id: movie.id,
-      original_language: movie.original_language,
-      original_title: movie.original_title,
+      originalLanguage: movie.original_language,
+      originalTitle: movie.original_title,
       overview: movie.overview,
       popularity: movie.popularity,
-      poster_path: `${imagesBaseUrl}${movie.poster_path}`,
-      release_date: movie.release_date,
+      posterPath: `${imagesBaseUrl}${movie.poster_path}`,
+      releaseDate: movie.release_date,
       title: movie.title,
-      vote_average: movie.vote_average,
-      vote_count: movie.vote_count
+      voteAverage: movie.vote_average,
+      voteCount: movie.vote_count
     }
 
   }
