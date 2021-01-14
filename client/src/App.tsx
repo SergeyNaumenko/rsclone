9e533d95b6b173c1942f71433b9fd703b550d6f6
Sergey Naumenko <sergeynau@gmail.com>
2021-01-14 03:45:23 +0300

    feat: add context for service

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,40 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Header } from './components/header.commponent';
 import { Footer } from './components/footer.component';
 import MovieApiService from './services/movieApiService';
+import { MovieApiServiceProvider } from './components/movie-api-service-context';
 
-const App: React.FC = () => {
+export default class App extends Component {
+  state = {
+    movieApiService: new MovieApiService()
+  };
+  
+  render() {
+    return (
+      <div className="App">
+        <Header/>
+        <MovieApiServiceProvider value={this.state.movieApiService} >
 
-  const clickHandler = async() => {
-    const res = await fetch('http://localhost:5000/api/auth/reg',{
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({login: 'atgbt',password: '33333'})
-    });
-    const data = await res.json();
-    console.log(data);
-    return data;
+        </MovieApiServiceProvider>
+        <Footer/>
+      </div>
+    );
   }
-
-  const movieApiService = new MovieApiService();
-  const genres = movieApiService.getGenres();
-  console.log(genres)
-
-  const topRated = movieApiService.getTopRatedMovies();
-  console.log(topRated)
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        init commit
-      </header>
-      <button onClick={clickHandler}>Fetch data</button>
-      <Header/>
-      <Footer/>
-    </div>
-  );
 };
-
-export default App;
---
+++ client/src/components/movie-api-service-context/movie-api-service-context.ts
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const {
+  Provider : MovieApiServiceProvider,
+  Consumer : MovieApiServiceConsumer
+} = React.createContext({});
+
+export {
+  MovieApiServiceProvider,
+  MovieApiServiceConsumer
+};
