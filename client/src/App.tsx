51824cee219224d8edb7f2a681a0b2c55c688ad2
Sergey Naumenko <sergeynau@gmail.com>
2021-01-21 00:35:08 +0300

    feat: add homepage features

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Header } from './components/header.component';
 import { Footer } from './components/footer.component';
 import MovieApiService from './services/movieApiService';
 import { MovieApiServiceProvider } from './components/movie_service_context';
+import { HomepageComponent } from './pages';
 import { GenresList } from './components/movieDB-lists';
 
 export default class App extends Component {
@@ -14,9 +16,19 @@ export default class App extends Component {
     return (
       <div className="App">
         <Header/>
-        <MovieApiServiceProvider value={this.state.movieApiService} >
-          <GenresList/>
-        </MovieApiServiceProvider>
+        <main className="row">
+          <MovieApiServiceProvider value={this.state.movieApiService} >
+            <Router>
+              <Switch>
+                <Route path="/" exact component={HomepageComponent}/>
+                <Route path="/list/:listName?" exact component={HomepageComponent}/>
+
+                <Route path="/genres/:genre?" exact component={GenresList}/>
+                <Route render={() => <h2>Page not found</h2>} />
+              </Switch>
+            </Router>
+          </MovieApiServiceProvider>
+        </main>
         <Footer/>
       </div>
     );
---
+++ client/src/pages/home.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { withRouter, Link } from 'react-router-dom';
+import ItemList from '../components/item-list';
+import { GenresList } from '../components/movieDB-lists';
+
+interface pageProps{
+  history:any,
+  match: any,
+}
+
+const HomepageComponent = ({ history, match}:pageProps) => {
+  const { listName } = match.params;
+  const listItems = [
+    {
+      id: 0,
+      name: 'latest',
+      displayName: 'Latest',
+      path: <GenresList/>,
+    },
+    {
+      id: 1, 
+      name: 'popular',
+      displayName: 'Popular',
+      list: <GenresList/>,
+    },
+    {
+      id: 2,
+      name: 'top',
+      displayName: 'Top Rated',
+      list: <GenresList/>,
+    },
+    {
+      id: 3,
+      name: 'upcoming',
+      displayName: 'Upcoming',
+      list: <GenresList/>,
+    }
+  ];
+
+  const listItem: any = listItems.find((item) => item.name === listName);
+  const listComponent: React.FC | React.Component = listItem ? listItem.list : null;
+  
+  const onItemSelected = function(id:number) {
+    history.push(id)
+  };
+
+  return (
+    <div className='container'>
+      <div className='row'>
+        <div className='col l2 s5'>
+          <div className="section">
+            <h5>Movies</h5>
+            <ItemList data={listItems} onItemSelected={onItemSelected}>
+              {
+                ({ displayName, name }:{displayName:string, name:string}) => <Link to={`/list/${name}`}>{ displayName }</Link>
+              }
+            </ItemList>
+            <div className="divider"></div>
+            <p><Link to='/genres'>Genres</Link></p>
+          </div>
+        </div>
+        <div className='col l10 s7'>{ listComponent }</div>
+      </div>
+    </div>
+  )
+}
+
+export default withRouter(HomepageComponent);
